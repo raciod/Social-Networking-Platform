912045175e3333676256d1a9c2cc7f3c23b1db4b
raciod <rachidactivity@gmail.com>
2024-02-27 16:44:39 +0100

    final project update finishing the loging and signup page

--- website/auth.py
+++ website/auth.py
@@ -1,19 +1,80 @@
-from flask import Blueprint, render_template, redirect, url_for
-
+from flask import Blueprint, render_template, redirect, url_for, request, flash
+from . import db
+from .models import User
+import re
+from flask_login import login_user, logout_user, login_required, current_user
+from werkzeug.security import generate_password_hash, check_password_hash
 
 auth = Blueprint("auth", __name__)
 
 
-@auth.route("/login")
+@auth.route("/login", methods=['GET','POST'])
 def login():
+    if request.method == 'POST':
+        email = request.form.get("email")
+        password = request.form.get("password")
+
+        user = User.query.filter_by(email=email).first()
+        if user:
+            if check_password_hash(user.password, password):
+                flash("Logged in!", category='success')
+                login_user(user, remember=True)
+                return redirect(url_for('views.home'))
+            else:
+                flash('Password is incorrect.', category='error')
+        else:
+            flash('Email does not exist.', category='error')
+
     return render_template("login.html")
 
 
-@auth.route("/sign-up")
+@auth.route("/sign-up", methods=['GET','POST'])
 def sign_up():
+    if request.method == 'POST':
+        # get data
+        email = request.form.get("email")
+        username = request.form.get("username")
+        password1 = request.form.get("password1")
+        password2 = request.form.get("password2")
+
+        # check email not in database
+        email_exists = User.query.filter_by(email=email).first()
+        username_exists = User.query.filter_by(username=username).first()
+        email_pattern = r'^[\w\.-]+@[\w\.-]+\.\w+$'
+
+        if email_exists:
+            flash('Email is already in use.', category='error')
+
+        # check user name not in database
+        elif username_exists:
+            flash('Username is already in use.', category='error')
+        
+        # check password
+        elif password1 != password2:
+            flash('Password don\'t match!', category='error')
+        elif len(password1) < 6:
+            flash('Password is too short.', category='error')
+        
+        elif len(username) < 2:
+            flash('Username is too short.', category='error')
+        
+        elif len(email) < 4 or not re.match(email_pattern, email):
+            flash("Invalid email address.", category='error')
+        
+        
+        else:
+            # create account
+            new_user = User(email=email, username=username, password=generate_password_hash(password1, method='pbkdf2:sha256'))
+            db.session.add(new_user)
+            db.session.commit()
+            login_user(new_user, remember=True)
+            flash('account created!')
+            return redirect(url_for('views.home'))
+        
     return render_template("signup.html")
 
-
 @auth.route("/logout")
+@login_required
 def logout():
+    logout_user()
     return redirect(url_for("views.home"))
